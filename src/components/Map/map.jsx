bea027cbec59b428b0f5fb22b1239e691db3df7a
Gabrielaso <gabrielaorozco17@gmail.com>
2026-09-20 12:59:35 -0700

    map shows markers using AdvancedMarker

--- src/components/Map/map.jsx
+++ src/components/Map/map.jsx
@@ -1,29 +1,56 @@
 import React from 'react';
-import { APIProvider, Map, AdvancedMarker} from '@vis.gl/react-google-maps';
+import { APIProvider, Map, AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
 import "./map.css";
 import marker01 from "../../assets/marker01.png";
+import data from "./crime_hotspots_mexico_temp.json";
+import scheme from "../../Schemas/location-schema.json"
 
+console.log(data)
+
+const pois = data.map((record, index) => ({
+    key: `poi-${index}`, 
+    location: {
+        lat: record.incident_location.coordinates.latitude,
+        lng: record.incident_location.coordinates.longitude
+    }
+}));
+
+const PoiMarkers = ({ pois }) => {
+    return (
+        <>
+            {
+                pois.map((poi) => (
+                    <AdvancedMarker
+                        key={poi.key}
+                        position={poi.location}>
+                        <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'
+
+                        } />
+                    </AdvancedMarker>
+                ))
+            }
+        </>
+    );
+};
 
 export default function MapComponent() {
     const position = { lat: 21.5042, lng: -104.8946 }; // Coordenadas de ejemplo (Nayarit)
 
     return (
         <div className="container">
-            <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+            <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
                 <div className='map'>
                     <Map
                         defaultCenter={position}
                         defaultZoom={20}
                         gestureHandling={'greedy'}
                         disableDefaultUI={false}
-                        mapId={import.meta.env.VITE_MAP_ID}
+                        mapId={import.meta.env.VITE_GOOGLE_MAPS_ID}
                     >
-                        {/* <AdvancedMarker className="marker" position={position}>
-                            <img src={{ marker01 }} alt="Custom Marker"/>
-                        </AdvancedMarker> */}
+                        <PoiMarkers pois={pois}/>
                     </Map>
                 </div>
-            </APIProvider >
+            </APIProvider>
         </div >
     )
 }
